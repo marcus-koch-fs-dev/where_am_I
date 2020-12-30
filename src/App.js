67c49fd83d9fa883f2cc6f90266ad3cb6dad291a
marcus-koch <marcus.koch01@outlook.de>
2020-12-30 23:11:37 +0100

    added prettier, eslint

--- src/App.js
+++ src/App.js
@@ -1,82 +1,86 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Spinner from "react-bootstrap/Spinner";
-import MapBox from "./components/MapBoxContainer";
-import InfoWindow from "./components/InfoWindow";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Spinner from 'react-bootstrap/Spinner'
+import MapBox from './components/MapBoxContainer'
+import InfoWindow from './components/InfoWindow'
+import './App.css'
 
 function App() {
-  const [coordinates, setCoordinates] = useState({ longitude: 0, latitude: 0 });
+  const [coordinates, setCoordinates] = useState({ longitude: 0, latitude: 0 })
   // const [userMarker, setUserMarker] = useState({ longitude: 0, latitude: 0 });
-  const [extraCountryInfo, setExtraCountryInfo] = useState(null);
-  const [spinnerEnabled, setSpinnerEnabled] = useState(false);
-  const [positionData, setPositionData] = useState(null);
+  const [extraCountryInfo, setExtraCountryInfo] = useState(null)
+  const [spinnerEnabled, setSpinnerEnabled] = useState(false)
+  const [positionData, setPositionData] = useState(null)
 
   useEffect(() => {
     const getPositionByBrowser = () => {
-      const error = (err) =>{}
+      const error = () => {}
       const option = {
         enableHighAccuracy: true,
         timeout: 5000,
-        maximumAge:10000
-      };
-      
+        maximumAge: 10000,
+      }
+
       if (!navigator.geolocation) {
-        window.message("Geolocation is not supported by your browser");
-      } 
-      else {
+        window.message('Geolocation is not supported by your browser')
+      } else {
         try {
-          navigator.geolocation.getCurrentPosition((position) => {
-            setCoordinates({
-              longitude: position.coords.longitude,
-              latitude: position.coords.latitude,
-            });
-            // setUserMarker({
-            //   longitude: position.coords.longitude,
-            //   latitude: position.coords.latitude,
-            // });
-            
-          }, error, option);
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setCoordinates({
+                longitude: position.coords.longitude,
+                latitude: position.coords.latitude,
+              })
+              // setUserMarker({
+              //   longitude: position.coords.longitude,
+              //   latitude: position.coords.latitude,
+              // });
+            },
+            error,
+            option
+          )
         } catch {
-          console.log(error.message);
+          console.log(error.message)
         }
       }
-    };
-    getPositionByBrowser();
-  }, []);
+    }
+    getPositionByBrowser()
+  }, [])
 
   useEffect(() => {
     const nameOfPosition = async () => {
-      setSpinnerEnabled(true);
-      let geoReverseResults;
-      
+      setSpinnerEnabled(true)
+      let geoReverseResults
+
       try {
-        const { REACT_APP_GeoIpFy_API_KEY } = process.env;
+        const { REACT_APP_GeoIpFy_API_KEY } = process.env
         const { latitude } = coordinates
         const { longitude } = coordinates
         geoReverseResults = await axios(
           `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&limit=1&apiKey=${REACT_APP_GeoIpFy_API_KEY}`
-        );
-        setPositionData(geoReverseResults.data.features[0]);
+        )
+        setPositionData(geoReverseResults.data.features[0])
       } catch (error) {
-        console.log(error.message);
+        console.log(error.message)
       }
 
       try {
-        const { country } = geoReverseResults.data.features[0].properties;
+        const { country } = geoReverseResults.data.features[0].properties
         // console.log(country);
         const extraCountryData = await axios(
           `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
-          );
-        setExtraCountryInfo(extraCountryData);
+        )
+        setExtraCountryInfo(extraCountryData)
       } catch (error) {
         // console.log(error.message);
         // alert("This place has no specific information. Choose a populated region for better results. ")
       }
-      setSpinnerEnabled(false);
-    };
-   coordinates.latitude !== 0 && coordinates.longitude !== 0 && nameOfPosition();
-  }, [coordinates]);
+      setSpinnerEnabled(false)
+    }
+    coordinates.latitude !== 0 &&
+      coordinates.longitude !== 0 &&
+      nameOfPosition()
+  }, [coordinates])
 
   // console.log(coordinates !== userMarker);
   return (
@@ -88,10 +92,10 @@ function App() {
         />
       )}
       {spinnerEnabled && <Spinner animation="border" />}
-      {coordinates && 
-        <MapBox coordinates={coordinates} setCoordinates={setCoordinates}/>
-      }
+      {coordinates && (
+        <MapBox coordinates={coordinates} setCoordinates={setCoordinates} />
+      )}
     </div>
-  );
+  )
 }
-export default App;
+export default App
--- src/components/MapBoxContainer.js
+++ src/components/MapBoxContainer.js
@@ -1,45 +1,43 @@
-import React from "react";
+import React from 'react'
 import ReactMapboxGl, {
   Marker,
   ScaleControl,
   ZoomControl,
   RotationControl,
-} from "react-mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+} from 'react-mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 export default function MapBoxContainer(props) {
-  const { REACT_APP_MAPBOX_API_KEY_A } = process.env;
-  const { REACT_APP_MAPBOX_API_KEY_B } = process.env;
-  const { REACT_APP_MAPBOX_API_KEY_C } = process.env;
-  const { setCoordinates } = props;
-  const latMap = props.coordinates.latitude;
-  const lngMap = props.coordinates.longitude;
+  const { REACT_APP_MAPBOX_API_KEY_A } = process.env
+  const { REACT_APP_MAPBOX_API_KEY_B } = process.env
+  const { REACT_APP_MAPBOX_API_KEY_C } = process.env
+  const { setCoordinates } = props
+  const latMap = props.coordinates.latitude
+  const lngMap = props.coordinates.longitude
   // const latMarker = props.userMarker.latitude
   // const lngMarker = props.userMarker.longitude
   // console.log("MapBox",props.coordinates,latMap,lngMap,latMarker,lngMarker);
 
   const Map = ReactMapboxGl({
     accessToken: `${REACT_APP_MAPBOX_API_KEY_A}.${REACT_APP_MAPBOX_API_KEY_B}.${REACT_APP_MAPBOX_API_KEY_C}`,
-  });
+  })
 
   return (
     <>
       <Map
-        // style={"mapbox://styles/mapbox/streets-v8"}
-        style={"mapbox://styles/mapbox/streets-v11"}
+        style="mapbox://styles/mapbox/streets-v11"
         containerStyle={{
-          height: "45vh",
-          width: "60vw",
-          opacity: "0.85",
+          height: '45vh',
+          width: '60vw',
+          opacity: '0.85',
         }}
         onClick={(map, clickedCoordinates) => {
           // console.log(clickedCoordinates)
           setCoordinates({
             longitude: clickedCoordinates.lngLat.lng,
             latitude: clickedCoordinates.lngLat.lat,
-          });
+          })
         }}
-        // onZoomEnd={(Map,zo)=>{console.log(zo)}}
         center={[lngMap, latMap]} // starting position [lng, lat]A
         scrollZoom={true}
         zooming={true}
@@ -56,7 +54,7 @@ export default function MapBoxContainer(props) {
         >
           <img
             src={
-              "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png"
+              'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png'
             }
             alt="marker"
             height="30vh"
@@ -65,5 +63,5 @@ export default function MapBoxContainer(props) {
       </Map>
       ;
     </>
-  );
+  )
 }
