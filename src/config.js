01545ba0af121d3c84ba078148250ff3482d7c1f
marcus-koch <marcus.koch01@outlook.de>
2021-01-03 00:25:02 +0100

    added ip detection when browser is not available, fixed bugs

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ export const config = {}
 
 config.mapbox = {}
 config.geoIpFy = {}
-config.restCountries = {}
+config.ipIfy = {}
 
 config.mapbox.KeyA = process.env.REACT_APP_MAPBOX_API_KEY_A
 config.mapbox.KeyB = process.env.REACT_APP_MAPBOX_API_KEY_B
 config.mapbox.KeyC = process.env.REACT_APP_MAPBOX_API_KEY_C
 config.geoIpFy.Key = process.env.REACT_APP_GeoIpFy_API_KEY
-config.restCountries.Key = process.env.REACT_APP_API_KEY
+config.ipIfy.Key = process.env.REACT_APP_IpIfy_API_KEY
