b2d39ed37c9073d08ddf377d868984cbb9c55ff0
marcus-koch <marcus.koch01@outlook.de>
2021-11-04 01:03:15 +0100

    Add: Home dummy component

---
+++ src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css'
+import { InfoCard } from '../../components/index'
+import { MapBoxCard } from '../../components/mapBox/index'
+
+const Home = () => {
+  return (
+    <>
+      <InfoCard />
+      <MapBoxCard />
+    </>
+  )
+}
+
+export default Home
